bb05334a20f73d00d15d5072503f6b5efccb2fc9
alamin147 <alamin.14780@gmail.com>
2025-08-06 17:39:27 +0600

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,17 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Quizzz',
+  title: 'EmoStory',
   description: 'This is a quiz application',
   generator: 'faiza',
+  icons: {
+    icon: [
+      {
+        url: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">🎮</text></svg>',
+        type: 'image/svg+xml',
+      },
+    ],
+  }
 }
 
 export default function RootLayout({
